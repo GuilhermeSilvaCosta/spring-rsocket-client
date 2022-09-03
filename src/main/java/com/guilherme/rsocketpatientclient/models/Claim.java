120e74752d3c3328c5cff560a44053d928c26d73
Guilherme Costa <guilhermecosta@MacBook-Pro-de-Guilherme.local>
2022-09-03 16:56:48 -0300

    First commit

---
+++ src/main/java/com/guilherme/rsocketpatientclient/models/Claim.java
@@ -0,0 +1,25 @@
+package com.guilherme.rsocketpatientclient.models;
+
+public class Claim {
+    
+    private float amount;
+    private String service;
+    
+    public float getAmount() {
+        return amount;
+    }
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+    public String getService() {
+        return service;
+    }
+    public void setService(String service) {
+        this.service = service;
+    }
+    public Claim(float amount, String service) {
+        this.amount = amount;
+        this.service = service;
+    }
+
+}
---
+++ src/test/java/com/guilherme/rsocketpatientclient/RsocketpatientclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.rsocketpatientclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketpatientclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
